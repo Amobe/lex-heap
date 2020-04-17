0b8dfa6483e787cb2ca9ba0b28ef939c309377ea
Lex Liu <amobe.momoko@gmail.com>
2020-04-18 03:40:59 +0800

    Implement min heapify for entire tree

--- src/heap/bheap.go
+++ src/heap/bheap.go
@@ -5,6 +5,11 @@ import (
 	"strings"
 )
 
+const (
+	_HeapSize = 256
+	_RootIdx  = 1
+)
+
 type binaryHeap struct {
 	tree []int
 	len  int
@@ -12,11 +17,13 @@ type binaryHeap struct {
 
 func NewBinaryHeap(in []int) Heap {
 	h := &binaryHeap{
-		tree: make([]int, 256),
+		tree: make([]int, _HeapSize),
 	}
 	for _, v := range in {
 		h.Insert(v)
 	}
+	h.MinHeapify()
+	fmt.Printf("%s\n", h)
 	return h
 }
 
@@ -31,8 +38,8 @@ func (h *binaryHeap) Poll() int {
 		return 0
 	}
 
-	v := h.tree[1]
-	for i := 1; i < h.len; i++ {
+	v := h.tree[_RootIdx]
+	for i := _RootIdx; i < h.len; i++ {
 		h.swapIdx(i, i+1)
 	}
 	h.len--
@@ -40,16 +47,85 @@ func (h *binaryHeap) Poll() int {
 }
 
 func (h *binaryHeap) Remove(v int) {
-
+	// TODO: imp
 }
 
 func (h *binaryHeap) String() string {
-	s := strings.Join(strings.Fields(fmt.Sprint(h.tree[:h.len])), " ")
+	s := strings.Join(strings.Fields(fmt.Sprint(h.tree[_RootIdx:h.len+1])), " ")
 	return s
 }
 
 func (h *binaryHeap) swapIdx(idxA, idxB int) {
+	if h.idxOutOfRange(idxA) || h.idxOutOfRange(idxB) {
+		return
+	}
 	tmp := h.tree[idxA]
 	h.tree[idxA] = h.tree[idxB]
 	h.tree[idxB] = tmp
+}
+
+func (h *binaryHeap) smallValueIdx(idxA, idxB int) int {
+	if h.idxOutOfRange(idxA) || h.emptyNode(idxA) {
+		return idxB
+	}
+	if h.idxOutOfRange(idxB) || h.emptyNode(idxB) {
+		return idxA
+	}
+	if h.tree[idxA] < h.tree[idxB] {
+		return idxA
+	}
+	return idxB
+}
+
+func (h *binaryHeap) minHeapify(root int) {
+	if h.idxOutOfRange(root) || h.emptyNode(root) {
+		return
+	}
+	left := h.leftChildIdx(root)
+	right := h.rightChildIdx(root)
+	if root != h.smallValueIdx(root, left) {
+		h.swapIdx(root, left)
+	}
+	if root != h.smallValueIdx(root, right) {
+		h.swapIdx(root, right)
+	}
+	h.minHeapify(left)
+	h.minHeapify(right)
+}
+
+func (h *binaryHeap) MinHeapify() {
+	rootIdx := 1
+	h.minHeapify(rootIdx)
+}
+
+func (h *binaryHeap) parentIdx(i int) int {
+	idx := i / 2
+	if h.idxOutOfRange(idx) || h.idxOutOfRange(i) {
+		return 0
+	}
+	return idx
+}
+
+func (h *binaryHeap) leftChildIdx(i int) int {
+	idx := i * 2
+	if h.idxOutOfRange(idx) || h.idxOutOfRange(i) {
+		return 0
+	}
+	return idx
+}
+
+func (h *binaryHeap) rightChildIdx(i int) int {
+	idx := i*2 + 1
+	if h.idxOutOfRange(idx) || h.idxOutOfRange(i) {
+		return 0
+	}
+	return idx
+}
+
+func (h *binaryHeap) emptyNode(i int) bool {
+	return i > h.len
+}
+
+func (h *binaryHeap) idxOutOfRange(i int) bool {
+	return i >= _HeapSize || i <= 0
 }
--- src/heap/bheap_test.go
+++ src/heap/bheap_test.go
@@ -24,3 +24,48 @@ func (s *BHeapSuite) TestNewBHeap() {
 
 	s.Equal(0, h.Poll())
 }
+
+func (s *BHeapSuite) TestBuildMinHeap() {
+	givin := []int{3, 5, 1, 4, 2}
+	h := NewBinaryHeap(givin)
+
+	expect := []int{1, 2, 3, 5, 4}
+	for _, v := range expect {
+		s.Equal(v, h.Poll())
+	}
+
+	s.Equal(0, h.Poll())
+}
+
+func (s *BHeapSuite) TestParentIdx() {
+	h := binaryHeap{}
+	s.Equal(0, h.parentIdx(0)) // out of range
+	s.Equal(0, h.parentIdx(1)) // out of range
+	s.Equal(1, h.parentIdx(3))
+	s.Equal(62, h.parentIdx(125))
+	s.Equal(63, h.parentIdx(126))
+	s.Equal(63, h.parentIdx(127))
+	s.Equal(64, h.parentIdx(128))
+}
+
+func (s *BHeapSuite) TestLeftChildIdx() {
+	h := binaryHeap{}
+	s.Equal(0, h.leftChildIdx(0)) // out of range
+	s.Equal(2, h.leftChildIdx(1))
+	s.Equal(6, h.leftChildIdx(3))
+	s.Equal(250, h.leftChildIdx(125))
+	s.Equal(252, h.leftChildIdx(126))
+	s.Equal(254, h.leftChildIdx(127))
+	s.Equal(0, h.leftChildIdx(128)) // out of range
+}
+
+func (s *BHeapSuite) TestRightChildIdx() {
+	h := binaryHeap{}
+	s.Equal(0, h.rightChildIdx(0)) // out of range
+	s.Equal(3, h.rightChildIdx(1))
+	s.Equal(7, h.rightChildIdx(3))
+	s.Equal(251, h.rightChildIdx(125))
+	s.Equal(253, h.rightChildIdx(126))
+	s.Equal(255, h.rightChildIdx(127))
+	s.Equal(0, h.rightChildIdx(128)) // out of range
+}
